4650b1b6fefe93ecf4f4b16ea76fdf13b30e43cc
anshumanGaikwad14 <gaanshuman14@gmail.com>
2026-07-14 02:11:29 +0530

    Prismic Implementation

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
+require("dotenv").config();
+
 const path = require("path");
 
 const express = require("express");
 const app = express();
 const port = 3000;
+
+// PRISMIC
+const prismic = require("@prismicio/client");
+
+const client = prismic.createClient(process.env.PRISMIC_REPOSITORY_NAME, {
+  accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+});
+
+// EXPRESS
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
@@ -15,8 +26,23 @@ app.get("/gallery", (req, res) => {
   res.render("pages/gallery");
 });
 
-app.get("/about", (req, res) => {
-  res.render("pages/about");
+app.get("/about", async (req, res) => {
+  try {
+    const [meta, about] = await Promise.all([
+      client.getSingle("meta"),
+      client.getSingle("about"),
+    ]);
+
+    console.log(meta, about, "✅✅");
+
+    res.render("pages/about", {
+      meta,
+      about,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error.message);
+  }
 });
 
 app.get("/collections", (req, res) => {
